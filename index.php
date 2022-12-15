4683cc65742b3d825b99c1c0018da19b1e0be0ec
Alelisk91 <alessandrofabbretti91@gmail.com>
2022-12-15 17:23:17 +0100

    Aggiunto file functions e incluso nel file index

--- index.php
+++ index.php
@@ -1,14 +1,5 @@
 <?php
-function randomPassword() {
-    $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
-    $pass = array();
-    $alphaLength = strlen($alphabet) - 1;
-    for ($i = 0; $i < $_GET['pass-leng']; $i++) {
-        $n = rand(0, $alphaLength);
-        $pass[] = $alphabet[$n];
-    }
-    return implode($pass);
-}
+include __DIR__ . "/functions.php"
 ?>
 
 <!DOCTYPE html>
